8ec7de1efd82c51046eed898da8ff045b73fbfd1
Emir Ibrahimbegovic <ibrahimbegovic@informz.com>
2017-01-16 17:26:49 -0500

    Revert to default config

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-APP_INSTANCE_NUMBER = ENV['APP_INSTANCE_NUMBER']
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
