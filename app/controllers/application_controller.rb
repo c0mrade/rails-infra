8b6c7d2f95f42cdc147c33f7e33d19e07c0a81e7
Emir Ibrahimbegovic <ibrahimbegovic@informz.com>
2017-01-16 18:10:27 -0500

    Bigger cache timeout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   def index
-    Rails.cache.fetch('some_data', expires_in: 2.seconds) do
+    Rails.cache.fetch('some_data', expires_in: 5.seconds) do
       'some data inside'
     end
     render text: 'Request landed at app instance'
