702704f6f52bed4330b38c63b681646ca0f41956
Emir Ibrahimbegovic <ibrahimbegovic@informz.com>
2017-01-16 18:30:53 -0500

    Worker logging

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -31,4 +31,23 @@ after_fork do |server, worker|
     ActiveRecord::Base.establish_connection
     Rails.logger.info('Connected to ActiveRecord')
   end
+
+  # This log hack provides separate log files for each unicorn worker.
+  # Since Unicorn forks worker processes after loggers are already initialized, by this point other
+  # things (like ActiveRecord::Base.logger) are already pointing directly at the current
+  # Rails.logger instance so we can't just point `Rails.logger` elsewhere.
+  logdev = Rails.logger.instance_variable_get(:@logdev)
+
+  ext = File.extname(logdev.dev.path)
+  path = logdev.dev.path.gsub /#{Regexp.escape(ext)}$/, ".#{worker.nr}#{ext}"
+
+  # open the file in the same way rails does:
+  #   https://github.com/rails/rails/blob/4606e75/railties/lib/rails/application/bootstrap.rb#L38-L40
+  file = File.open(path, 'a')
+  file.binmode
+  file.sync = Rails.application.config.autoflush_log
+
+  logdev.dev.flush
+  logdev.dev.close
+  logdev.instance_variable_set(:@dev, file)
 end
